a59e1e6c661d0307d4be74971295f498101c9618
Sara Johnsson <sara86persson@hotmail.com>
2024-05-02 19:09:18 +0200

    installed react, vite and redux, and cleaned the code

---
+++ to-do-app/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+// import { configureStore } from '@reduxjs/toolkit'
+
+// const store = configureStore({
+
+// });
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
