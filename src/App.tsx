f74a3e9f7106ff0a8ef021999b717685b0a7c346
Sara Johnsson <sara86persson@hotmail.com>
2024-05-14 18:38:05 +0200

    added a register page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import SignInComponent from './components/SignInComponent'
 import TodoComponent from './components/TodoComponents'
+import RegisterComponent from './components/RegisterComponent'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<SignInComponent />} />
+        <Route path='/register' element={<RegisterComponent />} />
         <Route path='/addtodo' element={<TodoComponent />} />
       </Routes>
     </Router>
--- src/components/RegisterComponent.tsx
+++ src/components/RegisterComponent.tsx
@@ -1,47 +1,45 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../features/firebaseConfig";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 
-const SignInComponent = () => {
+
+const RegisterComponent = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
 
 
-    const signInUser = async () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                console.log('Inloggad', userCredential);
+
+    const createUser = async () => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCrediential) => {
+                console.log('Inloggad', userCrediential);
                 navigate('/addtodo');
+
             })
             .catch((error) => {
                 console.log(error.message);
+
             });
     }
-
-
 
     return (
         <>
             <section>
-                <h2>Logga in</h2>
+                <h2>Registrera nytt konto</h2>
                 <label>Epost:</label>
                 <input type="text" placeholder="Firstname" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <br />
                 <label>Lösenord:</label>
                 <input type="password" placeholder="Lastname" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <br />
-                <button onClick={signInUser}>SignIn </button>
-                <br />
-                <Link to='/register'>Registrera nytt konto</Link>
-
-                <br />
+                <button onClick={createUser}>SignUp</button>
             </section>
         </>
     )
 }
 
-export default SignInComponent;
+export default RegisterComponent;
